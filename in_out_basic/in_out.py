4ca44fcd177d44106f9509fbf879c52b267094f2
C. Costa <ccosta@estgl.ipv.pt>
2022-10-25 15:30:02 +0100

    sum implement

---
+++ in_out_basic/in_out.py
@@ -0,0 +1,16 @@
+# input a float value for variable a here
+a = input('Introduza um número para a? ')
+a=float(a)
+print('Introduziu', a, ' para a')
+
+# input a float value for variable b here
+b = float(input('Introduza um número para b? '))
+print('Introduziu', b, ' para b')
+
+# output the result of addition here
+print(a, '+',b, '=', a +b)
+# output the result of subtraction here
+# output the result of multiplication here
+# output the result of division here
+
+print("\nThat's all, folks!")
